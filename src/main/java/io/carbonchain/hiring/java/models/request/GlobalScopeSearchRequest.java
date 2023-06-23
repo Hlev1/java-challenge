c72766b53d4f8b8d04edf2280ce4648d6cb65283
harrylevick <harry.levick@pleo.io>
2023-06-23 21:45:00 +0100

    create `GlobalScopeSearchRequest`

---
+++ src/main/java/io/carbonchain/hiring/java/models/request/GlobalScopeSearchRequest.java
@@ -0,0 +1,7 @@
+package io.carbonchain.hiring.java.models.request;
+
+public class GlobalScopeSearchRequest extends SearchRequest {
+    public GlobalScopeSearchRequest(String commodity) {
+        super(commodity);
+    }
+}
